21f889046bdb4df91fc98a3d3a86810b4e9f6f32
themahalocreative <88605523+themahalocreative@users.noreply.github.com>
2024-12-24 12:52:12 +1030

    ok

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+//wait until document is ready/loaded to run jquery- safety net for best practice
+$(document).ready(function() {
+    //mobile menu
+    $(".hamburger-button").click(function() {
+        $(this).toggleClass("active");
+        $(".mobile-menu").fadeToggle()
+    });
+    
+});
